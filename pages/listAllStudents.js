0acac84d8db4526c291b0347e1b9be16963ab0d2
nikolinafurlan <nikolina.furlan@gmail.com>
2023-03-31 20:47:37 +0100

    tenth commit

--- pages/listAllStudents.js
+++ pages/listAllStudents.js
@@ -26,8 +26,41 @@ export default function ListAllStudents({ data }) {
     }
   };
 
+    /**********CUSTOM THEME */
+    const myCustomTheme = createTheme({
+      type: 'light', // it could be "light" or "dark"
+      theme: {
+        colors: {
+          // brand colors
+          primaryLight: '$green200',
+          primaryLightHover: '$green300',
+          primaryLightActive: '$green400',
+          primaryLightContrast: '$green600',
+          primary: '#d8d8d8',
+          primaryBorder: '$green500',
+          primaryBorderHover: '$green600',
+          primarySolidHover: '$green700',
+          primarySolidContrast: '$white',
+          primaryShadow: '$green500',
+  
+          gradient:
+            'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+          link: '#5E1DAD',
+  
+          // you can also create your own color
+          myColor: '#ff4ecd',
+  
+          // ...  more colors
+        },
+        space: {},
+        fonts: {},
+      },
+    });
+  
+  
+
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={myCustomTheme}>
 
                     {/** Logo to appear on top */}
                     <Image width={320} height={180} src="atlaslogo.png" alt="def image" />
